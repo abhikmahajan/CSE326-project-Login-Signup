890f0b0822db824fa6f0b9dfa9738499f53bd6e8
abhikmahajan <abhikmahajan01@gmail.com>
2023-11-27 20:03:07 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+good_credit = True
+if good_credit:
+    print("you need to put down 10%")
+    print("Down payment:$100000")
+else:
+     print("you need to put down 20%")
+     print("Down payment:$200000")
