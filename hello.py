ccecc34857f3ae03fff745435ef4188f66b4902e
Abhik Mahajan <149783981+abhikmahajan@users.noreply.github.com>
2023-11-27 20:07:13 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-good_credit = True
-if good_credit:
-    print("you need to put down 10%")
-    print("Down payment:$100000")
-else:
-     print("you need to put down 20%")
-     print("Down payment:$200000")
